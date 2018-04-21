1225088c817ef9c8c9e0c03e32892814e780d6d4
Ryan Manchester <ryanm17@gmail.com>
2018-04-21 20:47:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,24 @@
 class Student
+attr_accessor :name, :grade
+attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+def initialize(name, grade, id=nil)
+  @id = id
+  @name = name
+  @grade = grade
+end
+
+def self.create_table
+  sql =  <<-SQL
+    CREATE NEW TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
+
 end
