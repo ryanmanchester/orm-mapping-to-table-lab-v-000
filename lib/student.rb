48f1e2b29f2a02ba6a48e021dce59910452bfe44
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 20:07:01 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 attr_accessor :name, :grade
 attr_reader :id
@@ -10,7 +11,7 @@ end
 
 def self.create_table
   sql =  <<-SQL
-    CREATE NEW TABLE IF NOT EXISTS students (
+    CREATE TABLE students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
@@ -19,6 +20,25 @@ def self.create_table
     DB[:conn].execute(sql)
   end
 
+  def self.drop_table
+    sql =  "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+   end
+
+   def self.create(name:, grade:)
+     student = Student.new(name, grade)
+     student.save
+     student
+   end
 
 
 end
