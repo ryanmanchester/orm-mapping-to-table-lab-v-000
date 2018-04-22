948aa1504cbf84e3d6f824139222714f9e94e9a6
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 19:29:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,13 @@ def self.create_table
     DB[:conn].execute(sql)
   end
 
+  def save
+    sql = <<-SQL
+     INSERT INTO students (name, grade)
+     VALUES (?, ?)
+     SQL
+     DB[:conn].execute(sql, self.name, self.grade)
+   end
 
 
 end
