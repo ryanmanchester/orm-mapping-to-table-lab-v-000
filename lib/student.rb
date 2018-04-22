729819edb12df555a23328ea130a8afa0ec66f42
Ryan Manchester <ryanm17@gmail.com>
2018-04-22 19:25:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,17 @@ end
 
 def self.create_table
   sql =  <<-SQL
-    CREATE TABLE students ( 
+    CREATE TABLE students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade INTEGER
     )
     SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql =  "DROP TABLE students"
     DB[:conn].execute(sql)
   end
 
